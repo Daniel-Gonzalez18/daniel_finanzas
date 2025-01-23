6191a38342ca2bc6a25d2c1b6c1a4b711d8d416d
Daniel-Gonzalez18 <dgonzun1812@g.educaand.es>
2025-01-23 10:02:14 +0100

    tarea 1

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -5,19 +5,31 @@
                 @foreach ($tableData['heading'] as $heading)
                 <th scope="col" class="px-6 py-3">
                     {{$heading}}
-                </th>    
+                </th>
                 @endforeach
             </tr>
         </thead>
         <tbody>
-            @foreach ($tableData['data'] as $row)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            @foreach ($tableData['data'] as $index=>$row)
+            @if($index%2===0)
+            <tr class="bg-gray-800 border-b dark:bg-gray-800 dark:border-gray-700">
+                @else
+            <tr class="bg-gray-300 border-b dark:bg-gray-800 dark:border-gray-700">
+                @endif
                 @foreach ($row as $cell)
-                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        {{$cell}}
-                    </th>    
+                @if ($cell==$row[0])
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$cell}}
+                </th>
+                @else
+                <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{$cell}}
+                </td>
+
+                @endif
+
                 @endforeach
-            </tr>    
+            </tr>
             @endforeach
         </tbody>
     </table>
